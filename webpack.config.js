66f39a4157e1d50f13ed5ab0b13326e60a9bff90
ivanchen0529 <57256864+ivanchen0529@users.noreply.github.com>
2021-03-26 11:41:47 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     mode: 'development',   //development(开发)  production(用户)
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthash].js'
-    }
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'My App',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
+
