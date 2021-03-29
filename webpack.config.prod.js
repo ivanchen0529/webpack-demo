2a1e88fc85208604f0c87151ee81390a688d1f8d
ivanchen0529 <57256864+ivanchen0529@users.noreply.github.com>
2021-03-29 14:24:54 +0800

    准备上传了

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,24 +1,22 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-
+const path = require("path");
 const base = require('./webpack.config.base.js')
 
 module.exports = {
     ...base,
-    mode: 'production',   //development(开发)  production(用户)
+    mode: "production",
     plugins: [
         ...base.plugins,
         new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
             filename: '[name].[contenthash].css',
             chunkFilename: '[id].[contenthash].css',
-            ignoreOrder: false,
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
         }),
     ],
     module: {
         rules: [
+            ...base.module.rules,
             {
                 test: /\.css$/i,
                 use: [
@@ -30,8 +28,7 @@ module.exports = {
                     },
                     'css-loader',
                 ],
-            },
-        ],
-    },
+            }
+        ]
+    }
 };
-
